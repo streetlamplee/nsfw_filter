5310046b14f390bbe4a1d58d7346074ae65daf79
Hyeon Jin Lee <jinintg@gmail.com>
2026-01-11 17:14:57 +0900

    feat: nsfw_filter 객체 멤버 함수 수정

--- nsfw_filter.py
+++ nsfw_filter.py
@@ -2,24 +2,25 @@ from transformers import pipeline
 from PIL import Image
 
 class Nsfw_filter:
-    model = None
-    def load_model():
+    classifier = None
+    def __init__(self):
+        return
+    def load_model(self):
         """서버 시작 시 모델을 로드합니다."""
-        global classifier
         print("Loading AI Model... (This may take a while for the first time)")
         try:
             # 모델은 docker-compose에 정의된 볼륨 경로에 저장됩니다.
-            classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
+            self.classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
             print("Model loaded successfully!")
         except Exception as e:
             print(f"Failed to load model: {e}")
 
 
-    def predict_image(image:Image):
-        if classifier is None:
+    def predict_image(self, image:Image):
+        if self.classifier is None:
             raise Exception("Model is not ready yet.")
 
         # 추론 수행
-        results = classifier(image)
+        results = self.classifier(image)
         
         return results
